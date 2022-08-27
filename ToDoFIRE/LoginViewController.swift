c7ae0aa92d18fd3f28fdf5fa2a225a6e7bf06cf6
Nikolai Maximov <kolkamaximov@gmail.com>
2022-08-27 16:08:54 +0300

    firebase notification

--- ToDoFIRE/LoginViewController.swift
+++ ToDoFIRE/LoginViewController.swift
@@ -21,7 +21,38 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+        
+    }
+    
+    @objc func kbDidShow(notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        
+        // размер клавиатуры
+        let kbFrameSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        // высота клавиатуры
+        (self.view as! UIScrollView).contentSize = CGSize(
+            width: self.view.bounds.width,
+            height: self.view.bounds.height + kbFrameSize.height
+        )
+        
+        (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(
+            top: 0,
+            left: 0,
+            bottom: kbFrameSize.height,
+            right: 0
+        )
+        
+    }
+    
+    @objc func kbDidHide() {
+        (self.view as! UIScrollView).contentSize = CGSize(
+            width: self.view.bounds.width,
+            height: self.view.bounds.height
+        )
     }
 
     @IBAction func loginButtonPressed(_ sender: UIButton) {
